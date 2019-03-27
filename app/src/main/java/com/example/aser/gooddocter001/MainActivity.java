21a35bd733f26931453e0093a0692eb2bd297952
DeLL <mudassar76642@gmail.com>
2019-03-27 17:47:09 +0500

    login implemebted

--- app/src/main/java/com/example/aser/gooddocter001/MainActivity.java
+++ app/src/main/java/com/example/aser/gooddocter001/MainActivity.java
@@ -1,25 +1,43 @@
 package com.example.aser.gooddocter001;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-    EditText eEid ,ePassword ;
+    EditText lEi ,lPassword ;
     Button bLogin;
+    private ProgressDialog progressDialog;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        eEid = findViewById(R.id.eeid);
-        ePassword = findViewById(R.id.epassword);
+        lEi = findViewById(R.id.lmail);
+        lPassword = findViewById(R.id.lpassword);
         bLogin= findViewById(R.id.blogin);
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this);
+        if(firebaseAuth.getCurrentUser() != null)
+        {
+            finish();
+            openHomePage();
+        }
 
 
     }
@@ -29,9 +47,42 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, SignUp.class);
         startActivity(intent);
     }
-    public void openHomePage(View view)
+    public void openHomePage()
     {
         Intent intent = new Intent(this, homepage.class);
         startActivity(intent);
+    }
+    public void userLogin(View view)
+    {
+        String Email = lEi.getText().toString().trim();
+        String Pass = lPassword.getText().toString().trim();
+        if(TextUtils.isEmpty(Email))
+        {
+            //email is empty
+            Toast.makeText(this,"Email Can't be left Blank",Toast.LENGTH_LONG).show();
+            return;
+        }
+        if(TextUtils.isEmpty(Pass))
+        {
+            //email is empty
+            Toast.makeText(this,"Enter the Password",Toast.LENGTH_LONG).show();
+            return;
+        }
+        //both the edit text are not empty
+        progressDialog.setMessage("loging in ...");
+        progressDialog.show();
+        firebaseAuth.signInWithEmailAndPassword(Email,Pass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
+                if(task.isSuccessful())
+                {finish();
+                    openHomePage();
+                }
+
+
+            }
+        });
+
     }
 }
