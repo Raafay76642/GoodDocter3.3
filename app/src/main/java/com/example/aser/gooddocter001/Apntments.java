6b06ac829843504177c174c65d9ba42651d98f7a
DeLL <mudassar76642@gmail.com>
2019-03-27 22:19:37 +0500

    login implemebted

--- app/src/main/java/com/example/aser/gooddocter001/Apntments.java
+++ app/src/main/java/com/example/aser/gooddocter001/Apntments.java
@@ -6,6 +6,12 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.sinch.android.rtc.ClientRegistration;
+import com.sinch.android.rtc.Sinch;
+import com.sinch.android.rtc.SinchClient;
+import com.sinch.android.rtc.SinchClientListener;
+import com.sinch.android.rtc.SinchError;
+
 public class Apntments extends AppCompatActivity {
     private TabLayout tablayout;
     private AppBarLayout appBarLayout;
@@ -25,5 +31,36 @@ public class Apntments extends AppCompatActivity {
         adapter.Addfragment(new Booked_apntments(), "Booked");
         viewPager.setAdapter(adapter);
         tablayout.setupWithViewPager(viewPager);
+
+    }
+    public void sinchstart(){
+        // Instantiate a SinchClient using the SinchClientBuilder.
+        android.content.Context context = this.getApplicationContext();
+        SinchClient sinchClient = Sinch.getSinchClientBuilder().context(context)
+                .applicationKey("c54d5c2b-3f3a-4ed8-8fe3-4cbdee39a664")
+                .applicationSecret("xTGt8qS7Z0SaxeheDFuQAA==")
+                .environmentHost("clientapi.sinch.com")
+                .userId("11")
+                .build();
+        // Specify the client capabilities.
+// At least one of the messaging or calling capabilities should be enabled.
+        sinchClient.setSupportCalling(true);
+        sinchClient.setSupportManagedPush(true);
+        sinchClient.addSinchClientListener(new SinchClientListener() {
+
+            public void onClientStarted(SinchClient client) { }
+
+            public void onClientStopped(SinchClient client) { }
+
+            public void onClientFailed(SinchClient client, SinchError error) { }
+
+            public void onRegistrationCredentialsRequired(SinchClient client, ClientRegistration registrationCallback) { }
+
+            public void onLogMessage(int level, String area, String message) { }
+        });
+
+        sinchClient.start();
+        sinchClient.stopListeningOnActiveConnection();
+        sinchClient.terminate();
     }
 }
